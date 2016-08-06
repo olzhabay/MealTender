3ea9682e181ed0db1e941820213a359347d48046
Olzhas Kaiyrakhmet <olzhabay.i@gmail.com>
2016-08-05 20:32:29 -0700

    change models

--- app/models.py
+++ app/models.py
@@ -7,8 +7,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User)
     phone_number = models.IntegerField()
-    restaurants = models.ForeignKey('Restaurant', blank=True, null=True)
-    address = models.ForeignKey('Address', blank=True, null=True)
+    restaurant = models.OneToOneField('Restaurant', blank=True, null=True)
 
     def __unicode__(self):
         return self.user.username
@@ -19,22 +18,48 @@ class Address(models.Model):
     zip_code = models.CharField(max_length=10)
     city = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
+    lat = models.FloatField(default=0.0)
+    lng = models.FloatField(default=0.0)
+
+    def __unicode__(self):
+        return str(self.lat) + str(self.lng)
 
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
-    address = models.ForeignKey('Address')
+    address = models.ForeignKey('Address', blank=True, null=True)
     menu = models.ManyToManyField('Food')
+
+    def __unicode__(self):
+        return self.name
 
 
 class Food(models.Model):
     name = models.CharField(max_length=100)
-    servings = models.IntegerField()
-    price = models.IntegerField()
+    spiciness = models.IntegerField(default=0)
+    is_noodle = models.BooleanField(default=False)
+    is_soup = models.BooleanField(default=False)
+    is_bibimbab = models.BooleanField(default=False)
+    is_pizza = models.BooleanField(default=False)
+    is_beef = models.BooleanField(default=False)
+    is_pork = models.BooleanField(default=False)
+    is_fish = models.BooleanField(default=False)
+    is_halal = models.BooleanField(default=False)
+    is_vegetarian = models.BooleanField(default=False)
+    servings = models.IntegerField(default=1)
+    price = models.IntegerField(default=0)
     ingredients = models.CharField(max_length=250)
+    image = models.ImageField(blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
 
 
 class Order(models.Model):
+    customer = models.ForeignKey('Profile', blank=True, null=True)
     total_price = models.IntegerField()
     food_list = models.ManyToManyField('Food')
-    shipping_address = models.ForeignKey('Address')
+    shipping_address = models.ForeignKey('Address', blank=True, null=True)
+
+    def __unicode__(self):
+        return self.customer.user.username
